840047c794e62c07ecd814697ce3d10b19208784
nalaudd <nalaudd@yahoo.com>
2020-10-12 21:27:33 -0500

    user class created

---
+++ TRMS/src/main/java/com/trms/users/User.java
@@ -0,0 +1,129 @@
+package com.trms.users;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+@Table(name = "USERS")
+public class User {
+
+	@Id
+	@SequenceGenerator(name = "USER_ID_SEQUENCE", sequenceName = "USER_ID_SEQUENCE")
+	@GeneratedValue(generator = "USER_ID_SEQUENCE", strategy = GenerationType.SEQUENCE)
+	private long id;
+	
+	@Column(unique = true)
+	private String username;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private String firstName;
+	
+	@Column
+	private String middleName;
+	
+	@Column
+	private String lastName;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private int phoneNumber;
+	
+	@Column
+	private boolean active;
+	
+	@Column
+	private boolean blocked;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public boolean isBlocked() {
+		return blocked;
+	}
+
+	public void setBlocked(boolean blocked) {
+		this.blocked = blocked;
+	}
+	
+}
