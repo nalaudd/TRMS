a99cf3ab3bfe0d993c28aed8c09e2b4f16f9f4a2
nalaudd <nalaudd@yahoo.com>
2020-10-12 20:47:13 -0500

    template project created

---
+++ TRMS/src/test/java/com/trms/TrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
